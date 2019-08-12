d99aa8609efd19a09ea79306cf4b82c26c9e683f
Angelica Maria Duque <angelidu@gmail.com>
2019-08-12 01:37:10 -0500

    removed firebase

--- build-code-exercise/src/index.js
+++ build-code-exercise/src/index.js
@@ -3,20 +3,15 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { reduxFirestore, getFirestore } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 import App from './components/App';
 import './assets/styles/App.scss';
 import rootReducer from './store/reducers/rootReducer';
-import fbConfig from './config/fbConfig';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   composeEnhancers(
-    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-    reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig)
+    applyMiddleware(thunk)
   )
 );
 
--- build-code-exercise/src/store/actions/authActions.js
+++ build-code-exercise/src/store/actions/authActions.js
@@ -1,19 +1,8 @@
-import { CREATE_USER, CREATE_USER_ERROR } from '../actionTypes';
+import { CREATE_USER} from '../actionTypes';
 
 // eslint-disable-next-line import/prefer-default-export
 export const createUser = user => {
-  return (dispatch, getState, { getFirebase, getFirestore }) => {
-    const firestore = getFirestore();
-    firestore
-      .collection('user')
-      .add({
-        ...user
-      })
-      .then(() => {
+  return (dispatch) => {
         dispatch({ type: CREATE_USER, user });
-      })
-      .catch(err => {
-        dispatch({ type: CREATE_USER_ERROR, err });
-      });
   };
 };
